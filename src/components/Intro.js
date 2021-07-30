a22788de12e6e16ab70d1277b7f9b3c7b13cf286
Eduardo Nahum <edunahum@gmail.com>
2021-07-30 16:47:16 -0400

    Update fade onload

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -61,13 +61,15 @@ const About = () => {
             </div>
           </Fade>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img
-            className="object-cover object-center rounded"
-            alt="hero"
-            src="./coding.svg"
-          />
-        </div>
+        <Fade>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img
+              className="object-cover object-center rounded"
+              alt="hero"
+              src="./coding.svg"
+            />
+          </div>
+        </Fade>
       </div>
     </section>
   );
